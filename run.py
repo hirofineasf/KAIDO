d0511646ff2e6777aa36db9baf7a98e20b02f5c6
Kaido <131613129+KAIDO-143@users.noreply.github.com>
2024-02-03 20:09:59 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os, sys, platform
 os.system('clear')
 try:os.system('rm -rf KAIDO')
 except:pass
-os.system("pkg uninstall python -y;pkg install python -y;pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
+#os.system("pkg uninstall python -y;pkg install python -y;pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
 try:os.system('git pull')
 except:pass
 bit = platform.architecture()[0]

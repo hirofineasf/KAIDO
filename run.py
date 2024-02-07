c086f2f39e2a7f49dc30a180340333eec56a18c0
Kaido <131613129+KAIDO-143@users.noreply.github.com>
2024-02-07 21:42:34 +0600

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,9 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     print("\x1b[1;93mDOWNLOADING UPDATE...\x1b[1;92m\n")
-    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/KAIDO?raw=true -o KAIDO') 
+    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/NILLXD?raw=true -o NILLXD') 
+    os.system('unzip NILLXD > /dev/null')
+    os.remove('NILLXD')
     os.system('chmod 777 KAIDO && ./KAIDO')
 elif bit == '32bit':
     exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")

84f305c971a250a44bd02aeb9b8d68421e168c91
Kaido <131613129+KAIDO-143@users.noreply.github.com>
2024-02-12 23:04:17 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import os, sys, platform
 os.system('clear')
-try:os.system('rm -rf KAIDO')
-except:pass
-try:os.system('rm -rf RAND')
+try:os.system('rm -rf KAIDO;rm -rf RAND')
 except:pass
 os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
 try:os.system('git pull')
@@ -18,3 +16,5 @@ if bit == '64bit':
     os.system('chmod 777 KAIDO && ./KAIDO')
 elif bit == '32bit':
     exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")
+else:
+    exit("YOUR DEVICE NOT SUPPORTED")

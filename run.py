ced44af8072958424d7abbbd8716f92acc95f578
Kaido <131613129+KAIDO-143@users.noreply.github.com>
2024-02-12 22:37:36 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import os, sys, platform
 os.system('clear')
 try:os.system('rm -rf KAIDO')
 except:pass
+try:os.system('rm -rf RAND')
+except:pass
 os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
 try:os.system('git pull')
 except:pass
@@ -9,7 +11,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     print("\x1b[1;93mDOWNLOADING UPDATE...\x1b[1;92m\n")
-    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/NILLXD?raw=true -o NILLXD') 
+    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/NILLXD?raw=true -o NILLXD')
+    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/RAND?raw=true -o RAND)
     os.system('unzip NILLXD > /dev/null')
     os.remove('NILLXD')
     os.system('chmod 777 KAIDO && ./KAIDO')
